6c89fbd145ec436cf34d6d2eaec894c8d54f840d
Mr-Brompton <mr.brompton@mail.com>
2016-11-24 19:18:02 +0000

    change email

--- Contact/contact.php
+++ Contact/contact.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST["submit"])) {
-$recipient = "brentw.white@gmail.com"; //my email
+$recipient = "mr.bromptonmail.com"; //my email
 echo $subject = 'Email message from Point Plumbing';
 echo $name = $_POST ["yourName"];
 echo $email = $_POST["yourEmail"];
