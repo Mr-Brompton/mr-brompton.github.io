e91be8943248e8a054b5cae9779aff2105055bc2
Mr-Brompton <mr.brompton@mail.com>
2017-03-28 00:30:11 +0100

    Game v1.2
    Unlimited Asteroids and score counter

--- myFirstGame/js/app.js
+++ myFirstGame/js/app.js
@@ -1,52 +1,61 @@
-var astYPositions = [0, -25, -50, -75, -100, -120, -130, -160, -200, -250, -300, -287, -145];
-var astXPositions = [250, 130, 300, 50, 190, 200, 225, 65, 10, 115, 75, 287, 300, 170, 10, 30, 60, 75, 85, 90, 110];
+var astYPositions = [];	//empty square brackets means new empty array
+var astXPositions = [];
 var rocketX = 0;
 var rocketY = 0;
-var rocketImg;
-var astImg;
-var numOfAst = astXPositions.length;
+var rocketImage;
+var astImage;
+var score = 0;
 
 function setUpGame() {
 	var gameCanvas = document.getElementById("gameCanvas");
-	rocketImg = new Image();
-	astImg = new Image();
-	rocketImg.src = "img/rocket.png";
-	astImg.src = "img/Ast.png";
+	rocketImage = new Image();
+	astImage = new Image();
+	astImage.src = "img/ast.png";
+	rocketImage.src = "img/rocket.png";
 	
-	gameCanvas.getContext("2d").drawImage(rocketImg, Math.random() * 100, Math.random() * 100);
-	gameCanvas.addEventListener("mousemove", mouseMove);
-	setInterval(onTick, 15);
+	gameCanvas.getContext("2d").drawImage(rocketImage, Math.random() * 100, Math.random() * 100);
+	
+	gameCanvas.addEventListener("mousemove", onMouseMovement);
+	setInterval(onTick, 10);
 }
 
-function mouseMove(mouseEvent) {
+function onMouseMovement(mouseEvent) {
 	rocketX = mouseEvent.offsetX;
 	rocketY = mouseEvent.offsetY;
 }
 
-
 function onTick() {
-    var gameCanvas = document.getElementById("gameCanvas");
-    var currentAstNumber = 0;
- 
-    while (currentAstNumber < numOfAst) {
-        astYPositions[currentAstNumber] = astYPositions[currentAstNumber] + 1;
-        currentAstNumber = currentAstNumber + 1;
-    }
-     
-    gameCanvas.width = 400;     //this erases the contents of the canvas
-    gameCanvas.getContext("2d").drawImage(rocketImg, rocketX, rocketY);
-     
-    currentAstNumber = 0;
-    while (currentAstNumber < numOfAst) {
-        gameCanvas.getContext("2d").drawImage(astImg, astXPositions[currentAstNumber], astYPositions[currentAstNumber]);
-        currentAstNumber = currentAstNumber + 1;
-    }
+	var gameCanvas = document.getElementById("gameCanvas");
+	var currentAstNumber = 0;
+	var numberOfEnemies = astXPositions.length;
 	
-currentAstNumber = 0;
-	while (currentAstNumber < numOfAst) {
-		if ( ( (rocketX < astXPositions[currentAstNumber] && astXPositions[currentAstNumber] < rocketX + 15) || (astXPositions[currentAstNumber] < rocketX && rocketX < astXPositions[currentAstNumber] + 15) ) && ( (rocketY < astYPositions[currentAstNumber] && astYPositions[currentAstNumber] < rocketY + 15) || (astYPositions[currentAstNumber] < rocketY && rocketY < astYPositions[currentAstNumber] + 15) ) ) {
-			alert("You hit an Asteroid!");
-		}
+	if (Math.random() < 1/20)
+	{
+		astYPositions.push(0);
+		astXPositions.push(Math.random() * 400);
+	}
+
+	while (currentAstNumber < numberOfEnemies) {
+		astYPositions[currentAstNumber] = astYPositions[currentAstNumber] + 1;
 		currentAstNumber = currentAstNumber + 1;
 	}
+	
+	gameCanvas.width = 400;		//this erases the contents of the canvas
+	gameCanvas.getContext("2d").drawImage(rocketImage, rocketX, rocketY);
+	
+	currentAstNumber = 0;
+	while (currentAstNumber < numberOfEnemies) {
+		gameCanvas.getContext("2d").drawImage(astImage, astXPositions[currentAstNumber], astYPositions[currentAstNumber]);
+		currentAstNumber = currentAstNumber + 1;
+	}
+	
+	currentAstNumber = 0;
+	while (currentAstNumber < numberOfEnemies) {
+		if ( ( (rocketX < astXPositions[currentAstNumber] && astXPositions[currentAstNumber] < rocketX + 17) || (astXPositions[currentAstNumber] < rocketX && rocketX < astXPositions[currentAstNumber] + 17) ) && ( (rocketY < astYPositions[currentAstNumber] && astYPositions[currentAstNumber] < rocketY + 17) || (astYPositions[currentAstNumber] < rocketY && rocketY < astYPositions[currentAstNumber] + 17) ) ) {
+			alert("You were destroyed by an asteroid!" + " you scored = " + score );
+		}
+		currentAstNumber = currentAstNumber + 1;
+	}	
+	
+	score = score + 1;
 }
