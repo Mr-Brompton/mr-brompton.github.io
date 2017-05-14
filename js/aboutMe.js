0a4ed52356da5fd11feb23925ee9008e2048b5c1
Mr-Brompton <mr.brompton@mail.com>
2017-05-14 15:04:32 +0100

    Upload Mr. Brompton Site Template

---
+++ js/aboutMe.js
@@ -0,0 +1,25 @@
+ function aboutMe() {
+    var x = document.getElementById('show');
+	if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+
+ function aboutMe2() {
+    var x = document.getElementById('show2');
+	if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+ function aboutMe3() {
+    var x = document.getElementById('show3');
+	if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
---
+++ js/blogContents.js
@@ -0,0 +1,25 @@
+ function topic() {
+    var x = document.getElementById('posts');
+	if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+
+ function aboutMe2() {
+    var x = document.getElementById('show2');
+	if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+ function aboutMe3() {
+    var x = document.getElementById('show3');
+	if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
