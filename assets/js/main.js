516f954b9d0b2e9ed76bd111f508c51657f06c22
mr-brompton <mr.brompton@mail.com>
2019-07-13 17:33:43 +0100

    Just a little tidy-up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 var quotes = [
 "Rowing harder doesn't help,</br> if you're rowing in the wrong direction.",
 "\"Everyone thinks they have a plan until they get punched in the face\"</br> - Mike Tyson",
-"'That's' not a password, </br>'Th!s !s 4 P455w0rd1;'",
+"'That's not a password, </br>'Th!s !s 4 P455w0rd1;'",
 "\"The computer is a moron\" </br> - Peter Drucker",
-"Cryptocurrencies: Everything you don't understand about money combined with everything you don't understand about computers\"</br> - John Oliver",
+"\"Cryptocurrencies: Everything you don't understand about money combined with everything you don't understand about computers\"</br> - John Oliver",
 "This aptitude does not have super cow powers",
 "Things have changed in the past two years\" </br>- Bill  Gates"
 ];
